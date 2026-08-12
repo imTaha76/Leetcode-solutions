078372d92b40bab2e5e05cd21d4f9139f6f983ed
Taha Ansari <tahansari6254@gmail.com>
2026-08-13 00:59:26 +0530

    File updated at 8/13/2026, 12:59:27 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,25 +1,14 @@
-// Last updated: 8/13/2026, 12:52:05 AM
-1import java.util.HashMap;
+// Last updated: 8/13/2026, 12:59:27 AM
+1import java.util.HashSet;
 2class Solution {
 3    public boolean containsDuplicate(int[] nums) {
 4        boolean result = false;
-5        HashMap<Integer , Integer> Counts = new HashMap<>();
+5        HashSet<Integer> Counts = new HashSet<Integer>();
 6        for(int i : nums){
-7            if(Counts.get(i) == null){
-8                Counts.put(i , 1);
+7            if(!Counts.add(i)){
+8                result = true;
 9            }
-10            else{
-11                Counts.put(i , Counts.get(i) + 1);
-12                result = true;
-13                break;
-14            }
-15        }
-16        // for(int j : nums){
-17        //     if(Counts.get(j) > 1){
-18        //         result = true;
-19        //         break;
-20        //     }
-21        // }
-22        return result;
-23    }
-24}
+10        }
+11        return result;
+12    }
+13}
