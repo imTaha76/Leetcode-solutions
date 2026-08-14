edbbf4f77017da91a494c053b4db08f9e27358e7
Taha Ansari <tahansari6254@gmail.com>
2026-08-15 02:23:28 +0530

    File updated at 8/15/2026, 2:23:28 AM

--- 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.java
+++ 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.java
@@ -1,34 +1,18 @@
-// Last updated: 8/15/2026, 2:21:19 AM
+// Last updated: 8/15/2026, 2:23:28 AM
 1class Solution {
 2    public long zeroFilledSubarray(int[] nums) {
 3        int n = nums.length;
-4        // int[] zero_count = new int[n];
-5        int count = 0;
-6        // int pointer = 0;
-7
-8        // for(int i =0; i < n; i++){
-9        //     if(nums[i] == 0){
-10        //         count++;
-11        //     }
-12        //     else{
-13        //         zero_count[pointer] = count;
-14        //         pointer++;
-15        //         count = 0;
-16
-17        //     }
-18        
-19        // }
-20        // count = 0;
-21        long sum = 0;
-22        for(int j =0; j<n;j++){
-23            if(nums[j] == 0){
-24              count++;
-25              sum = sum + count;  
-26            }
-27            else{
-28                count = 0;
-29            }
-30        }
-31        return sum;
-32    }
-33}
+4        int count = 0;
+5        long sum = 0;
+6        for(int j =0; j<n;j++){
+7            if(nums[j] == 0){
+8              count++;
+9              sum = sum + count;  
+10            }
+11            else{
+12                count = 0;
+13            }
+14        }
+15        return sum;
+16    }
+17}
