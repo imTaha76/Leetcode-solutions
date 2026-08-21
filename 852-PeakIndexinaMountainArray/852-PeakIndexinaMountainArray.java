65d09d041271d258a688885e73211bd5f8ac680d
Taha Ansari <tahansari6254@gmail.com>
2026-08-21 22:49:08 +0530

    Create 852-PeakIndexinaMountainArray

---
+++ 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.java
@@ -0,0 +1,24 @@
+// Last updated: 8/21/2026, 10:49:08 PM
+1class Solution {
+2    public int peakIndexInMountainArray(int[] arr) {
+3        int n = arr.length;
+4        int start = 0;
+5        int end = n-1;
+6        int ans = 0;
+7
+8        while(start <= end){
+9            int mid = (start + end) / 2;
+10            if(arr[mid] < arr[mid + 1]){
+11               
+12               start = mid + 1;
+13            }
+14            if(arr[mid] >= arr[mid + 1]){
+15                ans = mid;
+16                end = mid - 1;
+17            }
+18
+19
+20        }
+21        return ans;
+22    }
+23}
