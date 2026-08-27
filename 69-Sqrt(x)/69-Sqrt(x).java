75f4fbd2a7032512ea65b5fc7c48710470f01068
Taha Ansari <tahansari6254@gmail.com>
2026-08-27 18:20:04 +0530

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -0,0 +1,30 @@
+// Last updated: 8/27/2026, 6:20:04 PM
+1class Solution {
+2    public int mySqrt(int x) {
+3        
+4       if(x == 1){
+5        return 1;
+6       }
+7       if(x == 0){
+8        return 0;
+9       }
+10       int ans = 0;
+11
+12       int start = 1;
+13       int end = x/2;
+14       while (start <= end){
+15        int mid = start + (end - start)/2;
+16        if(mid == x/mid){
+17         return mid;
+18        }
+19        if(mid  > x/mid){
+20            end = mid - 1;
+21        }
+22        if(mid < x/mid){
+23            ans = mid;
+24            start = mid + 1;
+25        }
+26       }
+27       return ans;
+28    }
+29}
